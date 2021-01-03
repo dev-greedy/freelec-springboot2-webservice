fa2fc9bdfbed7a9b5136e91024670656894ef1c8
[seongmin_desktop] <thisisclaytoy@naver.com>
2021-01-03 11:02:00 +0900

    PostsApiController, PostsSaveRequestDto, PostsService 클래스 생

---
+++ src/main/java/com/seongmin/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.seongmin.book.springboot.service.posts;
+
+import com.seongmin.book.springboot.domain.posts.PostsRepository;
+import com.seongmin.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
